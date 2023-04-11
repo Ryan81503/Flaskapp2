ff26954b2616938dfe35907215277f51b49aa724
Ryan81503 <ryan.rodriguez09@utrgv.edu>
2023-04-11 12:08:40 -0500

    First commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+def readDetails(filepath):
+    with open(filepath, 'r') as f:
+        return [line for line in f]
+    
+@app.route("/")
+def homepage():
+  name = "Ryan Rodriguez"
+  details = readDetails('Static/details.txt')
+  return render_template("homepage.html", name=name, aboutMe = details)
+
+@app.route('/greet/<name>')
+def greet(name):
+  return f'<h1> Hello there, {name} </h1>'
+
+@app.route('/form', methods=['GET', 'POST'])
+def formDemo():
+    name = None
+    if request.method == 'POST':
+        name = request.form['name']
+
+    return render_template('form.html', name=name)
+  
+if __name__ == "__main__":
+  app.run(debug=True, port=2000)
